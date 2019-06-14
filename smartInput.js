bac1a9088bf56951e6b9ed361aa41af1eefc3ba0
yanglu09 <yanglu09@baidu.com>
2019-06-14 16:42:47 +0800

    自动分词去重

--- smartInput.js
+++ smartInput.js
@@ -19,7 +19,7 @@ Vue.component('smart-input', {
                 <ul class="smartInput-badge-list">
                     <li v-for="item in selected" class="smartInput-badge">
                         <span>{{item}}</span>
-                        <span class="smartInput-badge-icon" @click="deleteOne">
+                        <span class="smartInput-badge-icon" @click.stop="deleteOne">
                             <svg viewBox="64 64 896 896" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true" class=""><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path></svg>
                         </span>
                     </li>
@@ -109,6 +109,9 @@ Vue.component('smart-input', {
         init(e) {
             this.searching = true;
             this.$emit('focus');
+            if (this.searchString === this.selected[0]) {
+                this.filtered = this.list;
+            }
         },
         // 调整联想搜索面板的大小和位置
         initBadgeInput(e) {
@@ -175,8 +178,10 @@ Vue.component('smart-input', {
         },
         // 触发选中事件
         chooseOne(target) {
-            this.searchString = '';
             const value = target.innerText;
+            if (value.includes(this.searchString)) {
+                this.searchString = '';
+            }
             if (this.multiple) {
                 this.$refs.autoFocus.focus();
                 let selected = this.selected.slice();
@@ -203,6 +208,8 @@ Vue.component('smart-input', {
         },
         // 删除一个选项
         deleteOne(e) {
+            // 点击删除选项时清除blur的回调，保持下拉框显示
+            clearTimeout(this.timer);
             this.chooseOne(e.currentTarget.parentElement.children[0]);
         },
         // 键盘选择一个选项
@@ -229,11 +236,11 @@ Vue.component('smart-input', {
                     let invalidArr = [];
                     let flag = false;
                     arr.forEach(item => {
-                        if (this.list.includes(item)) {
+                        if (this.list.includes(item) && !this.selected.includes(item)) {
                             this.selected.push(item);
                             flag = true;
                         }
-                        else {
+                        else if (!this.list.includes(item)) {
                             invalidArr.push(item);
                         }
                     })
